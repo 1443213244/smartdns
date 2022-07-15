10cc456bb37f21618796ae6eda1d1c692ceef2d7
1443213244 <1443213244@qq.com>
2022-07-15 15:21:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ version=1.2022.04.05-2046.x86_64-linux-all
 wget https://github.com/pymumu/smartdns/releases/download/Release36.1/smartdns.$version.tar.gz
 tar xvf smartdns.$version.tar.gz
 cd smartdns && bash install -i
+wget -O /etc/smartdns/smartdns.conf https://raw.githubusercontent.com/1443213244/smartdns/main/config.conf 
+systemctl restart smartdns
