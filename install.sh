61764959724fbbf0c5a3d94107ab2fdafdebb9cd
1443213244 <1443213244@qq.com>
2022-07-15 15:24:59 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ tar xvf smartdns.$version.tar.gz
 cd smartdns && bash install -i
 wget -O /etc/smartdns/smartdns.conf https://raw.githubusercontent.com/1443213244/smartdns/main/config.conf 
 systemctl restart smartdns
+cd
+rm -rf smartdns*
